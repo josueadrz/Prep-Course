e9b84710c7706ce42cd0fff2855a985d492e44af
josueadrz <josueadrz@gmail.com>
2022-02-16 01:12:39 -0500

    ejericios desarrollados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,9 +175,9 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if (numero > 0){
-    return 'positivo';
+    return 'Es positivo';
   } else if (numero < 0){
-    return 'negativo';
+    return 'Es negativo';
   } else {
     return false;
   }
@@ -201,7 +201,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return `hola ${nombre}!`
+  return `Hola ${nombre}!`
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -241,7 +241,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length !===1) return "Dato incorrecto"
+  if (letra.length !==1) return "Dato incorrecto"
   
 }
 
